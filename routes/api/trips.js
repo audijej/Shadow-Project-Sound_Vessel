0aefd853b6bf2b99315ce92c3f8ac9c4fc98f92f
Jesse Audije <audijej@gmail.com>
2020-09-13 15:25:26 -0700

    updated seeds

--- routes/api/trips.js
+++ routes/api/trips.js
@@ -27,38 +27,7 @@ router.route("/:id").get(function(req, res) {
     }).then(function(dbTrip) {
         console.log("find the problem")
         res.json(dbTrip);
-        // let [trips] = [{dbTrip}]
-        // const resObj = trips.map(trip => {
-        //     return Object.assign(
-        //         {}, {
-        //            sail_date_id: trip.sail_date_id,
-        //            start_destination: trip.start_destination,
-        //            end_destination: trip.end_destination,
-        //            start_sail_date: trip.start_sail_date,
-        //            end_sail_date: trip.end_sail_date,
-        //            main_id: trip.main_id.map(ship => {
-        //                return Object.assign(
-        //                    {},
-        //                    {
-        //                     main_id: ship.main_id,
-        //                     ship_id: ship.ship_id,
-        //                     imo: ship.imo,
-        //                     mmsi: ship.mmsi,
-        //                     ship_name: ship.ship_name,
-        //                     callsign: ship.callsign,
-        //                     flag: ship.flag,
-        //                     ship_type_name: ship.ship_type_name,
-        //                     destination: ship.destination,
-        //                     ship_length: ship.ship_length,
-        //                     ship_width: ship.ship_width,
-        //                     eta: ship.eta
-        //                    }
-        //                )
-        //            })
-        //         }
-        //     )
-        // })
-        // res.json(resObj)
+
     });
 });
 //Trip route for saving a new trip
