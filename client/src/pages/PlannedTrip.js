0d3d0c22a26d498baf33fb64040c03617b640c63
Jesse Audije <audijej@gmail.com>
2020-09-19 02:20:45 -0700

    updated planned trip table and styling

--- client/src/pages/PlannedTrip.js
+++ client/src/pages/PlannedTrip.js
@@ -5,7 +5,8 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import "react-datepicker/dist/react-datepicker.css";
 import API from "../utils/API";
-import { List, ListItem } from "../components/List/List"
+import { List, ListItem } from "../components/List/List";
+import ListGroup from 'react-bootstrap/ListGroup';
 import { Link, useParams } from "react-router-dom";
 import SeattleLakeUnionBoat from '../components/Images/Seattle-Lake-Union.JPG';
 import Modal from 'react-bootstrap/Modal';
@@ -125,42 +126,48 @@ function PlannedTrip() {
 
                 <h1 style={{ textAlign: "center", fontSize: "50px" }}>Your Saved Trips</h1>
                 <br></br>
-                <h2>Start Destination: {trip.start_destination}</h2>
+                <table id="tripListing">
+                    <thead>
+                        <tr>
+                            <th>Start Destination:</th>
+                            <td>{trip.start_destination}</td>
+                        </tr>
+
+                        <tr>
+                            <th>End Destination:</th>
+                            <td>{trip.end_destination}</td>
+                        </tr>
+
+                        <tr>
+                            <th>Start Sail Date:</th>
+                            <td>{trip.start_sail_date && trip.start_sail_date.split("T")[0]}</td>
+                        </tr>
+
+                        <tr>
+                            <th>End Sail Date:</th>
+                            <td>{trip.end_sail_date && trip.end_sail_date.split("T")[0]}</td>
+                        </tr>
+
+                    </thead>
+
+                </table>
+                {/* <h2>Start Destination: {trip.start_destination}</h2>
                 <h2>End Destination: {trip.end_destination}</h2>
                 <h2>Start Sail Date: {trip.start_sail_date && trip.start_sail_date.split("T")[0]}</h2>
                 <h2> End Sail Date: {trip.end_sail_date && trip.end_sail_date.split("T")[0]}</h2>
-                <br></br>
+                <br></br> */}
                 <h1 style={{ color: "white", fontSize: "50px" }}><Link to="/plantrip">← Back to Plan a Trip</Link>
                 </h1>
 
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Start Destination</th>
-                            <th>End Destination</th>
-                            <th>Start Sail Date</th>
-                            <th>End Sail Date</th>
-                        </tr>
-                    </thead>
 
-                    <tbody>
-                        <tr>
-                            <td>{trip.start_destination}</td>
-                            <td>{trip.end_destination}</td>
-                            <td>{trip.start_sail_date && trip.start_sail_date.split("T")[0]}</td>
-                            <td>{trip.end_sail_date && trip.end_sail_date.split("T")[0]}</td>
-                        </tr>
-                    </tbody>
-
-                </table>
 
             </Container>
 
             <Container fluid id="resultsdiv" style={{ color: "black", textAlign: "center" }} >
-                <h1 >Ships encountered</h1>
+                <h1 style={{ textAlign: "center", color: "black", fontFamily: 'Kaushan Script', textShadow: "4px 4px 4px #000000" }}>Ships encountered</h1>
                 {trafficMatch.length > 0 ? (
 
-                    <List>
+                    <ListGroup>
 
                         {trafficMatch.filter((elem) => {
                             let start_sail_date = trip.start_sail_date && trip.start_sail_date.split("T")[0];
@@ -168,13 +175,13 @@ function PlannedTrip() {
                             return start_sail_date === eta
                         }).map((traffics) => {
                             console.log(traffics)
-                            return (<ListItem key={traffics.main_id} value={traffics}>
+                            return (<ListGroup.Item variant="info" key={traffics.main_id} value={traffics}>
                                 <strong>
                                     <Button variant="primary" onClick={() => handleShow(traffics)}>
                                         Ship Name: {traffics.ship_name}
                                     </Button>
                                 </strong>
-                            </ListItem>
+                            </ListGroup.Item>
                             )
                         })
 
@@ -182,7 +189,7 @@ function PlannedTrip() {
 
                         <Modal size="lg" show={show} onHide={handleClose}>
                             <Modal.Header closeButton>
-                                <Modal.Title style={{textAlign:"right", fontSize: "30px", fontWeight: "bolder"}}>{modalIdContents.ship_name}</Modal.Title>
+                                <Modal.Title style={{ textAlign: "right", fontSize: "30px", fontWeight: "bolder" }}>{modalIdContents.ship_name}</Modal.Title>
                             </Modal.Header>
                             <Modal.Body key={modalIdContents.main_id} value={modalIdContents}>
                                 <table>
@@ -199,8 +206,8 @@ function PlannedTrip() {
                                         </tr>
 
                                         <tr>
-                                           <th>Ship Type:</th>
-                                           <td>{modalIdContents.ship_type_name}</td>
+                                            <th>Ship Type:</th>
+                                            <td>{modalIdContents.ship_type_name}</td>
                                         </tr>
 
                                         <tr>
@@ -209,13 +216,13 @@ function PlannedTrip() {
                                         </tr>
 
                                         <tr>
-                                           <th>Destination:</th>
-                                           <td>{modalIdContents.destination}</td>
+                                            <th>Destination:</th>
+                                            <td>{modalIdContents.destination}</td>
                                         </tr>
 
                                         <tr>
-                                           <th>ETA:</th>
-                                           <td>{modalIdContents.eta && modalIdContents.eta.split("T")[0]}</td>
+                                            <th>ETA:</th>
+                                            <td>{modalIdContents.eta && modalIdContents.eta.split("T")[0]}</td>
                                         </tr>
                                     </thead>
                                 </table>
@@ -228,7 +235,7 @@ function PlannedTrip() {
                                 </Button>
                             </Modal.Footer>
                         </Modal>
-                    </List>
+                    </ListGroup>
                 ) : (
                         <h3>No Results to Display</h3>
                     )}
